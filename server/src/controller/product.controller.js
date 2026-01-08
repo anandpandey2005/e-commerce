17249c5b5341d0ab6e758e11c0dc774f1961a720
ANAND PANDEY <anandpandey20005@gmail.com>
2026-01-08 23:56:42 +0530

    ok

--- server/src/controller/product.controller.js
+++ server/src/controller/product.controller.js
@@ -1,8 +1,9 @@
-
+import { Product } from "../models/product.model.js";
 import { apiError } from "../utils/apiError.utils.js";
 import { apiResponse } from "../utils/apiResponse.utils.js";
 
-export const getProduct = async (req, res) => {
+//######################## Get All Products ##########################
+export const getAllProduct = async (req, res) => {
   try {
     const products = await Product.find().populate("category").lean();
 
@@ -22,6 +23,19 @@ export const getProduct = async (req, res) => {
     const statusCode = err.statusCode || 500;
     const message = err.message || "An internal server error occurred.";
     const errors = err.errors || [];
-    return apiError.errorResponse(res, statusCode, message, errors);
+    return apiError.erroRresponse(res, statusCode, message, errors);
+  }
+};
+
+//######################## Add New Product ##########################
+export const addNewProduct = async (req, res) => {
+  try {
+  } catch (err) {
+    console.error("Error in getProduct controller:", err.message, err.stack);
+
+    const statusCode = err.statusCode || 500;
+    const message = err.message || "An internal server error occurred.";
+    const errors = err.errors || [];
+    return apiError.erroRresponse(res, statusCode, message, errors);
   }
 };
--- server/src/models/product.model.js
+++ server/src/models/product.model.js
@@ -1,5 +1,7 @@
 import mongoose, { Schema } from "mongoose";
 
-const ProductSchema = new Schema({}, { timestamps: true });
+const ProductSchema = new Schema({
+    
+}, { timestamps: true });
 
 export const Product = mongoose.model("Product", ProductSchema);
