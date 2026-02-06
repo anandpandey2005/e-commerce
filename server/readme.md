e6b002452e1d2aa30dfca9e12b7b32c4a2293fbb
ANAND PANDEY <anandpandey20005@gmail.com>
2026-02-06 12:40:42 +0530

    eraser link attach

--- server/readme.md
+++ server/readme.md
@@ -1,2 +1,4 @@
 # TEST API ON POSTMAN 
 ## POSTMAN COLLECTION LINK : https://web.postman.co/workspace/My-Workspace~451e9438-e0ea-44e5-8191-681494f8c55c/collection/37758472-e0909dab-e2d7-4cf7-a8d2-4dc86b1e6154?action=share&source=copy-link&creator=37758472
+
+##Eraser link : https://app.eraser.io/workspace/jvOeonDdNH5dTzQaB8Ao
--- server/src/models/product.model.js
+++ server/src/models/product.model.js
@@ -1,7 +1,40 @@
 import mongoose, { Schema } from "mongoose";
 
-const ProductSchema = new Schema({
-    
-}, { timestamps: true });
+const SpecificationSchema = new Schema(
+  {
+    key: { type: String, required: true },
+    value: { type: String, required: true },
+  },
+  { _id: false }
+);
+
+const ProductSchema = new Schema(
+  {
+    image: [{ type: String, trim: true, default: null }],
+    name: {
+      type: String,
+      lowercase: true,
+      trim: true,
+      unique: true,
+      required: [true, "Name is required"],
+    },
+    model: {
+      type: String,
+      lowercase: true,
+      trim: true,
+      unique: true,
+      required: [true, "Model is required"],
+    },
+    description: { type: String, lowercase: true, trim: true },
+
+    specifications: [
+      {
+        category: { type: String, required: true },
+        details: [SpecificationSchema],
+      },
+    ],
+  },
+  { timestamps: true }
+);
 
 export const Product = mongoose.model("Product", ProductSchema);
